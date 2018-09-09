7b53b8229ee553228180a04bc7003fa11372b10a
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-09 23:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function updateObjectWithKeyAndValue (object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return Object.assign(object, {[key]:value});
 }
+
+function deleteFromObjectByKey (object, key) {
+  var newobj = Object.assign ({}, object);
+  delete newobj.key;
+  return newobj;
+}
