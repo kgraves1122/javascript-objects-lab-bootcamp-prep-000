5fefaa1a7976406838fb356bcc353c3842b62714
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-09 23:41:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue (object,key, value) {
+  object[key] = value;
+  return object;
+}
+
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign({}, object, {});
+}
